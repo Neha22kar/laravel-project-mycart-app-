a8cba98650e8e9be84647d6956bd083159a5d476
neha22kar <nehabawiskar22@gmail.com>
2023-11-21 12:07:01 +0530

    login & register api using passport.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 use App\Models\User;
@@ -11,38 +12,68 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => \Hash::make($request->password) 
         ]);
 
-        return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
+        $token = $user->createToken('Token')->accessToken;
+     //   return response()->json(['token'=>$token,'user'=>$user,'message' => 'User registered successfully'],200);
+        return response()->json(['user'=>$user,'message' => 'User registered successfully'],200);
     }
-
+    
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
+      $data = [
+        'email' =>$request->email,
+        'password' =>$request->password
+      ];
 
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $user = Auth::user();
-            $token = $user->createToken('authToken')->plainTextToken;
+      if(Auth()->attempt($data)){
+        $user = Auth::user();
+        $token = Auth()->user()->createToken('Token')->accessToken;
 
-            return response()->json(['user' => $user, 'token' => $token, 'message' => 'Login successful'], 200);
-        } else {
-            return response()->json(['message' => 'Invalid credentials'], 401);
-        }
-    }//
+        // return response()->json(['token'=>$token,'user' => $user,'message' => 'Login successful'],200);
+        return response()->json(['user' => $user,'message' => 'Login successful'],200);
+    }
+    else{
+        return response()->json(['error'=>'unauthorized'],401);
+    }
+    }
+    // public function register(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'email' => 'required|string|email|unique:users',
+    //         'password' => 'required|string|min:8',
+    //     ]);
+
+    //     $user = User::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'password' => Hash::make($request->password),
+    //     ]);
+
+    //     return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
+    // }
+
+    // public function login(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|string|email',
+    //         'password' => 'required|string',
+    //     ]);
+
+    //     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+    //         $user = Auth::user();
+    //         $token = $user->createToken('authToken')->plainTextToken;
+
+    //         return response()->json(['user' => $user, 'token' => $token, 'message' => 'Login successful'], 200);
+    //     } else {
+    //         return response()->json(['message' => 'Invalid credentials'], 401);
+    //     }
+    // }//
 }
--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function(){
+    Route::get('get-user',[AuthController::class,'userInfo']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
 
